d95b3995012bb565e46dea0184f8b02966b28284
RABBI-VAI-git <rabbiboss4037@gmail.com>
2025-10-13 17:58:16 +0600

    Update Help.js

--- Script/commands/Help.js
+++ Script/commands/Help.js
@@ -36,10 +36,10 @@ ${allCommands.map(cmd => `║ ➔ ${cmd}`).join("\n")}
 
  
  const backgrounds = [
- "https://i.imgur.com/ZRQpRGL.jpeg",
- "https://i.imgur.com/ZRQpRGL.jpeg",
- "https://i.imgur.com/ZRQpRGL.jpeg",
- "https://i.imgur.com/ZRQpRGL.jpeg"
+ "https://i.imgur.com/uGhfdDy.jpeg",
+ "https://i.imgur.com/uGhfdDy.jpeg",
+ "https://i.imgur.com/uGhfdDy.jpeg",
+ "https://i.imgur.com/uGhfdDy.jpeg"
  ];
  const selectedBg = backgrounds[Math.floor(Math.random() * backgrounds.length)];
  const imgPath = __dirname + "/cache/helpallbg.jpg";
